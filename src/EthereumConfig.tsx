37e51a356fa7176e4ceaf8cec98030a5b0ced7a6
Kirill Bulgakov <kbulgakov@exodus.io>
2023-05-01 16:27:16 +0300

    feat: add a working PoC

--- src/EthereumConfig.tsx
+++ src/EthereumConfig.tsx
@@ -4,8 +4,8 @@ import { mainnet } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 import { WagmiConfig } from "wagmi";
 import { RainbowKitProvider, connectorsForWallets } from "@rainbow-me/rainbowkit";
-import { metaMaskWallet } from "@rainbow-me/rainbowkit/wallets";
-import {useMemo} from "react";
+import { metaMaskWallet, coinbaseWallet, braveWallet } from "@rainbow-me/rainbowkit/wallets";
+import { useEffect, useMemo, useState } from 'react';
 
 const { chains, provider } = configureChains(
     [mainnet],
@@ -20,21 +20,38 @@ const { chains, provider } = configureChains(
     ],
 );
 
+const useWallets = () => {
+  const [wallets, setWallets] = useState([])
+
+  useEffect(() => {
+    setTimeout(() => {
+      // @ts-ignore
+      setWallets([coinbaseWallet, braveWallet])
+    }, 5000)
+  })
+
+  return wallets
+}
+
 function EthereumConfig({ children }: { children: React.ReactNode }) {
+  const wallets = useWallets()
+
   const connectors = useMemo(() => connectorsForWallets([
     {
       groupName: 'Recommended',
       wallets: [
+        // @ts-ignore
+        ...wallets.map(wallet => wallet({ chains })),
         metaMaskWallet({ chains })
       ],
     }]
-  ), [])
+  ), [wallets])
 
-  const wagmiClient = createClient({
+  const wagmiClient = useMemo(() => createClient({
     autoConnect: true,
     connectors,
     provider
-  })
+  }), [connectors])
 
   return (
       <WagmiConfig client={wagmiClient}>
